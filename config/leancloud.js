1cd3db7bd57573fd3d798d7442348881c6e55780
duyang <79229788@qq.com>
2016-08-27 21:51:02 +0800

    update

--- config/leancloud.js
+++ config/leancloud.js
@@ -1,5 +1,5 @@
 module.exports.leancloud = {
-  APP_ID : 'XjoYAdkHp1s3wsXxOd0br7sE',
-  APP_KEY: '1HrJQakITm0FHK8ls9v8oHQR',
-  MASTER_KEY:'XDjFjESiGi1QbBsWa44mu3eE'
+  APP_ID : '',
+  APP_KEY: '',
+  MASTER_KEY:''
 };
