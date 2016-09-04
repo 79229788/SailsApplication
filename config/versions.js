a74f0a86b3d08dc47291c8fabbced7b7702c9ca4
duyang <79229788@qq.com>
2016-09-04 22:54:33 +0800

    update

--- config/versions.js
+++ config/versions.js
@@ -1,10 +1,3 @@
 module.exports.versions = {
-	"/js/config.js":"5cb509bcf63b7c10",
-	"/js/views/page-home/main.js":"5fc3aed342c12281",
-	"/styles/common.css":"d2959364e7b56345",
-	"/styles/page-home.css":"49cb7fe6a0c83ac3",
-	"/styles/page.css":"3781ce6f0826febb",
-	"/styles/source.css":"d41d8cd98f00b204",
-	"/images/.gitkeep":"d41d8cd98f00b204",
-	"/templates/.gitkeep":"d41d8cd98f00b204"
+
 };
